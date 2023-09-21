67eb5fe01e7ff394ea568d389fb9d6c9762488a7
anithaa10 <144413094+anithaa10@users.noreply.github.com>
2023-09-21 09:00:02 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,19 @@
-def fact_rec(n):
-  if(n==0)or(n==1):
-    return 1
-  else:
-    return n*fact_rec(n-1)
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-number=int(input("Enter a value:"))
-res=fact_rec(number)
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-print("The factorial of {} is {}".format(number,res))
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects for Batsman and Bowler
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-def isLeapYear(year):
-  if(year % 4==0 and year % 100!=0) or year % 400 ==0:
-    return True
-  else:
-    return False
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-year = int(input("Enter a year : "))
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than 0.")
 
-if isLeapYear(year):
-  print(" {} is a leap year.". format(year))
-else:
-  print(" {} is not a leap year.". format(year))
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+
+# Test the BankAccount class
+if __name__ == "__main__":
+    # Create an instance of BankAccount
+    my_account = BankAccount("12345", "Mr.replit", 1000.0)
+
+    # Display initial balance
+    my_account.display_balance()
+
+    # Deposit money
+    my_account.deposit(500)
+
+    # Withdraw money
+    my_account.withdraw(200)
+
+    # Display updated balance
+    my_account.display_balance()
